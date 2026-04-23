ac91a589b0997e39ed15482c57c9b1c5b4838bb0
Renan Gimenez <renangiimenez@gmail.com>
2026-04-23 20:54:37 -0300

    feat: implement & customize tabs navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,34 @@
-import { Stack } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }}></Stack>;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarLabelPosition: "beside-icon",
+        tabBarInactiveTintColor: "#CECECE",
+        tabBarActiveTintColor: "#000",
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Sign In",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="home" size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="sign-up"
+        options={{
+          tabBarLabel: "Sign Up",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="person" size={20} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
