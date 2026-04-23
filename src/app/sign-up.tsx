32f8c1f178386838c52f543450bf8bbe9dbb8c63
Renan Gimenez <renangiimenez@gmail.com>
2026-04-22 23:46:30 -0300

    chore: implement router navigation e canGoBack method

--- src/app/sign-up.tsx
+++ src/app/sign-up.tsx
@@ -1,12 +1,20 @@
-import { Link } from "expo-router";
-import { StyleSheet, View } from "react-native";
+import { router } from "expo-router";
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function SignUp() {
+  function back() {
+    if (!router.canGoBack()) {
+      return Alert.alert("Não é possível voltar!");
+    }
+
+    router.back();
+  }
+
   return (
     <View style={styles.container}>
-      <Link href="/" style={styles.back}>
-        Voltar
-      </Link>
+      <TouchableOpacity onPress={back}>
+        <Text style={styles.back}>Voltar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
