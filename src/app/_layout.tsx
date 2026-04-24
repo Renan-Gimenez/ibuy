4494a69f105df374fc93fd1f6ed2b24c3b724e7e
Renan Gimenez <renangiimenez@gmail.com>
2026-04-23 21:02:19 -0300

    chore: install drawer

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,34 +1,5 @@
-import { MaterialIcons } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
+import { Drawer } from "expo-router/drawer";
 
 export default function Layout() {
-  return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarLabelPosition: "beside-icon",
-        tabBarInactiveTintColor: "#CECECE",
-        tabBarActiveTintColor: "#000",
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          tabBarLabel: "Sign In",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="home" size={20} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="sign-up"
-        options={{
-          tabBarLabel: "Sign Up",
-          tabBarIcon: ({ color }) => (
-            <MaterialIcons name="person" size={20} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
-  );
+  return <Drawer />;
 }
