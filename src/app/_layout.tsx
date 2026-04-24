1f6aadb7abed37e28694420100e3cefb98d05e71
Renan Gimenez <renangiimenez@gmail.com>
2026-04-23 21:12:52 -0300

    chore: customize drawer navigation

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,34 @@
+import { MaterialIcons } from "@expo/vector-icons";
 import { Drawer } from "expo-router/drawer";
 
 export default function Layout() {
-  return <Drawer />;
+  return (
+    <Drawer
+      screenOptions={{
+        drawerActiveTintColor: "#000",
+        drawerInactiveTintColor: "#999999",
+      }}
+    >
+      <Drawer.Screen
+        name="index"
+        options={{
+          title: "Entrar",
+          drawerLabel: "Entrar",
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="sign-up"
+        options={{
+          title: "Criar Conta",
+          drawerLabel: "Criar Conta",
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="add" size={size} color={color} />
+          ),
+        }}
+      />
+    </Drawer>
+  );
 }
