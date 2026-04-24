b2df0b164cbcd06f0f6cc04536e59c0f5f16471c
Renan Gimenez <renangiimenez@gmail.com>
2026-04-23 22:26:25 -0300

    feat: implement tabs

--- src/app/(drawer)/_layout.tsx
+++ src/app/(drawer)/_layout.tsx
@@ -1,9 +1,27 @@
+import { MaterialIcons } from "@expo/vector-icons";
 import { Drawer } from "expo-router/drawer";
 
 export default function DrawerLayout() {
   return (
     <Drawer>
-      <Drawer />
+      <Drawer.Screen
+        name="(tabs)"
+        options={{
+          drawerLabel: "Início",
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="configs"
+        options={{
+          drawerLabel: "Configurações",
+          drawerIcon: ({ color, size }) => (
+            <MaterialIcons name="settings" size={size} color={color} />
+          ),
+        }}
+      />
     </Drawer>
   );
 }
